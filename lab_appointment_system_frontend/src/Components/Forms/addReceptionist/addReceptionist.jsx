27309bd2fe1cdaf3ff55607fbe4bc1780d440766
Tharu60 <115988213+Tharu60@users.noreply.github.com>
2024-03-18 15:40:38 +0530

    Tables, Forms creating

---
+++ lab_appointment_system_frontend/src/Components/Forms/addReceptionist/addReceptionist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addReceptionist = () => {
+  return (
+    <div>addReceptionist</div>
+  )
+}
+
+export default addReceptionist
---
+++ lab_appointment_system_frontend/src/Components/Forms/addTechnician/addTechnician.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addTechnician = () => {
+  return (
+    <div>addTechnician</div>
+  )
+}
+
+export default addTechnician
